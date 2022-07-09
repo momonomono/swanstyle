faf31e4b777be60ac5db07c91549ce329bc523f1
momonomono <momonomono1128@gmail.com>
2022-07-09 09:22:40 +0900

    faviconを設定しました

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="shortcut icon" href="{{ asset('/img/favicon_swanstyle.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('/img/favicon.ico') }}">
     <meta name="keywords" content="エステ,多治見市,swanstyle,スワンタイル,小顔マッサージ">
-    <meta name="description"　content="スワンスタイルは多治見市のエステサロンです。小顔マッサージや毛穴洗浄、ホワイトニング、インナーマッスルトレーニングなどお客様に合わせたサービスをご用意しております">
+    <meta name="description" content="スワンスタイルは多治見市のエステサロンです。小顔マッサージや毛穴洗浄、ホワイトニング、インナーマッスルトレーニングなどお客様に合わせたサービスをご用意しております">
     <title>エステサロン SWANSTYLE 多治見市 </title>
+
+    <meta 
 </head>
 <body>
     <div id="app">
